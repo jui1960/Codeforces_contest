05db8cee3a7db1eb36b05e6e83a30f0cba0a517d
jui1960 <juifull196044@gmail.com>
2026-01-25 20:57:21 +0600

    update

--- A_DBMB_and_the_Array.cpp
+++ A_DBMB_and_the_Array.cpp
@@ -1,34 +1,64 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n, s, x;
+//         cin >> n >> s >> x;
+//         vector<int> v(n);
+//         int sum = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> v[i];
+//             sum += v[i];
+//         } 
+//        sum = s-sum;
+//         if (sum >= 0 && sum%x==0)
+//         {
+//             cout << "YES\n";
+//         }
+//         else
+//         {
+//             cout << "NO\n";
+//         }
+//     }
+
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     int t;
     cin >> t;
     while (t--)
     {
-        int n, s, x;
-        cin >> n >> s >> x;
-        vector<int> v(n);
+        int a, b, c;
+        cin >> a >> b >> c;
         int sum = 0;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < a; i++)
         {
-            cin >> v[i];
-            sum += v[i];
-        } 
-       sum = s-sum;
-        if (sum >= 0 && sum%x==0)
-        {
-            cout << "YES\n";
+            int num;
+            cin>>num;
+            sum+=num;
         }
-        else
-        {
-            cout << "NO\n";
+        if(sum<=b&&(b-sum)%c==0){
+            cout<<"YES\n";
         }
+        else cout<<"NO\n";
     }
-
     return 0;
 }
